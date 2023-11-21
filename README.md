0aee505801e8eb0058ecf2b4a92c3387193d3ef6
Iván Molina Rebolledo <ivan@ivmoreau.com>
2023-11-21 03:03:09 -0600

    Add OpenALPR.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Behold, an assemblage of Nix devshells, artfully wrought for the purposes of dev
 In the present hour, the following devshells have been duly fashioned for your perusal:
 
 - `rvc-webui`: An illustrious devshell tailored for the [rvc](https://github.com/RVC-Project/Retrieval-based-Voice-Conversion-WebUI) webui enterprise. It affords the distinguished privilege of executing the `run.sh` script, thereby setting the webui into operation, a task rendered as facile as a gentle zephyr's caress.
+
+## The Art of Packages
+
+The following packages have been artfully crafted for your perusal:
+
+- `openalpr`: A package for the [OpenALPR](https://github.com/openalpr/openalpr) project, which provides automatic license plate recognition. (Btw, IT DOES NOT WORK ON MACOS, but I have no idea why. It works on Linux, though. But in macOS the process is just killed without any error message.)
--- flake.nix
+++ flake.nix
@@ -11,6 +11,31 @@
         inputs.flake-parts.flakeModules.easyOverlay
       ];
       perSystem = { pkgs, lib, config, system, ... }: {
+        packages.openalpr = pkgs.stdenv.mkDerivation {
+          name = "openalpr";
+          src = pkgs.fetchFromGitHub {
+            owner = "openalpr";
+            repo = "openalpr";
+            rev = "736ab0e";
+            hash = "sha256-Nd/CP+3K/Du+ST6RJjLe0QFdZlmQP/tKmqfh+u7MUpA=";
+          };
+          patches = [
+            patches/0001-Correct-include-path-for-Tesseract.patch
+          ];
+          sourceRoot = "source/src";
+          nativeBuildInputs = with pkgs; [ 
+            tesseract3
+            leptonica
+            opencv3
+            curl
+            cmake
+            log4cplus
+          ];
+          cmakeFlags = [
+            "-DCMAKE_INSTALL_SYSCONFDIR:PATH=${placeholder "out"}/etc"
+          ];
+          meta.mainProgram = "alpr";
+        };
         devShells.rvc-webui = pkgs.mkShell.override { stdenv = pkgs.llvmPackages_15.libcxxStdenv; } {
           buildInputs = with pkgs; [
             python38
